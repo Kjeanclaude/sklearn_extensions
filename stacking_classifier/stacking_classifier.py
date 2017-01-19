b66cc7408e94ba677a8be08797712194cc36e5ab
Alex Zyryanov <alexander.zyryanov44@gmail.com>
2017-01-19 15:11:19 +0700

    stacking classifier realize fitting

--- stacking_classifier/stacking_classifier.py
+++ stacking_classifier/stacking_classifier.py
@@ -11,13 +11,76 @@ class StackingClassifier(BaseEstimator, ClassifierMixin):
                  estimators,
                  meta_classifier,
                  cv,
-                 use_proba=True,
-                 average_proba=False,
-                 verbose=0):
+                 use_original_features=False,
+                 use_probas=True,
+                 average_probas=False,
+                 verbose=2):
 
         self.estimators = estimators
         self.meta_classifier = meta_classifier
         self.cv = cv
-        self.use_proba = use_proba
-        self.average_proba = average_proba
+        self.use_original_features = use_original_features
+        self.use_probas = use_probas
+        self.average_probas = average_probas
         self.verbose = verbose
+
+    def _predict_meta_features(self, X):
+        if self.use_probas:
+            probas = np.asarray([est.predict_proba(X)
+                                 for est in self.ests_])
+            if self.average_probas:
+                vals = np.average(probas, axis=0)
+            else:
+                vals = np.concatenate(probas, axis=1)
+        else:
+            vals = np.column_stack([est.predict(X) for est in self.ests_])
+
+        if self.use_original_features:
+            vals = np.concatenate((X, vals), axis=1)
+
+        return vals
+
+    def _fit_estimators(self, X, y):
+        self.ests_ = [clone(est) for est in self.estimators]
+
+        if self.verbose > 0:
+            print("Fitting {} estimators...".format(len(self.ests_)))
+
+        for i, est in enumerate(self.ests_):
+            if self.verbose > 0:
+                print("Fitting estimators {est_i}: {est_name} ({est_i}/{est_count})"
+                      .format(est_i=i, est_name=type(est).__name__.lower(), est_count=len(self.ests_)))
+
+            if self.verbose > 1 and hasattr(est, 'verbose'):
+                est.set_params(verbose=self.verbose - 1)
+
+            est.fit(X, y)
+
+    def _fit_meta_features(self, X, y):
+        meta_features = None
+        for train_index, test_index in self.cv:
+            self._fit_estimators(X[train_index], y[train_index])
+            local_meta_features = self._predict_meta_features(X[test_index])
+
+            if meta_features is None:
+                meta_features = np.zeros((X.shape[0], local_meta_features.shape[1]))
+
+            meta_features[test_index] = local_meta_features
+
+        return np.concatenate(meta_features)
+
+    def _fit_meta_classifier(self, X, y):
+        if self.verbose > 0:
+            print("Fitting meta classifier {est_name}"
+                  .format(est_name=type(self.meta_clf_).__name__.lower()))
+
+        if self.verbose > 1 and hasattr(self.meta_clf_, 'verbose'):
+            self.meta_clf_.set_params(verbose=self.verbose - 1)
+
+        self.meta_clf_ = clone(self.meta_classifier)
+
+    def fit(self, X, y):
+        meta_features = self._fit_meta_features(X, y)
+        self._fit_meta_classifier(meta_features, y)
+
+        return self
