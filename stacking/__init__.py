5e386849cb507796c5f8bed754384abec959a7cc
Alex Zyryanov <alexander.zyryanov44@gmail.com>
2017-01-19 16:46:46 +0700

    fix __init__.py

--- stacking/__init__.py
+++ stacking/__init__.py
@@ -1,3 +1,7 @@
 from .stacking_estimator import StackingEstimator
+from .stacking_classifier import StackingClassifier
+from .stacking_regressor import StackingRegressor
 
-__all__ = ['StackingEstimator']
+__all__ = ['StackingEstimator',
+           'StackingClassifier',
+           'StackingRegressor']
