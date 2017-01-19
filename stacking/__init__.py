0c4a2a820905fe0728c10b06b332f56eb6b526d5
Alex Zyryanov <alexander.zyryanov44@gmail.com>
2017-01-19 16:35:07 +0700

    stacking classifier refactor to stacking estimator

---
+++ stacking/__init__.py
@@ -0,0 +1,3 @@
+from .stacking_estimator import StackingEstimator
+
+__all__ = ['StackingEstimator']
--- stacking_classifier/__init__.py
+++
@@ -1,3 +0,0 @@
-from .stacking_classifier import StackingEstimator
-
-__all__ = ['StackingEstimator']
