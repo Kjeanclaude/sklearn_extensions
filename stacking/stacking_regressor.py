633411639a31bdedcc35fb8dc891a846691c2eed
Alex Zyryanov <alexander.zyryanov44@gmail.com>
2017-01-20 13:14:40 +0700

    fix sklearn conventions

--- stacking/stacking_regressor.py
+++ stacking/stacking_regressor.py
@@ -4,6 +4,23 @@ from .stacking_estimator import StackingEstimator
 
 
 class StackingRegressor(StackingEstimator, RegressorMixin):
+    def __init__(self,
+                 estimators,
+                 meta_estimator,
+                 cv,
+                 use_original_features=False,
+                 use_probas=True,
+                 average_predictions=False,
+                 meta_features_preprocessing=lambda x: x,
+                 target_preprocessing=lambda x, y: y,
+                 verbose=0):
 
-    def __init__(self, *args, **kwargs):
-        super(StackingRegressor, self).__init__(*args, **kwargs)
+        super(StackingRegressor, self).__init__(estimators,
+                                                meta_estimator,
+                                                cv,
+                                                use_original_features,
+                                                use_probas,
+                                                average_predictions,
+                                                meta_features_preprocessing,
+                                                target_preprocessing,
+                                                verbose)
