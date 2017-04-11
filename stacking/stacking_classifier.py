b83475e51a27c043936a6fdc9ae562c6cb895068
Alexander <alexander.zyryanov44@gmail.com>
2017-04-12 01:38:15 +0600

    prepublish refactor

--- stacking/stacking_classifier.py
+++ stacking/stacking_classifier.py
@@ -30,6 +30,5 @@ class StackingClassifier(StackingEstimator, ClassifierMixin):
 
     def predict_proba(self, X):
         meta_features = self._predict_meta_features(X)
-        meta_features = self._preprocess_meta_features(meta_features)
 
         return self.meta_est_.predict_proba(meta_features)
